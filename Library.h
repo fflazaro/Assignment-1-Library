a7a1dfa872e62cb946a8e1ecf17232b352bfb1b0
Franchesca <59673723+fflazaro@users.noreply.github.com>
2022-03-31 11:13:52 -0700

    Header file and .cpp file

---
+++ Library.h
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std; 
+
+//Add a new book
+//Return true for sucess, false if book is already in library
+bool AddBook(const string &name); 
+
+//Remove a book
+//Return true for success, false if book is not in library
+bool RemoveBook(const string &name); 
+
+//List all books in library
+void ListAllBooks() const; 
+
+//Return true if book in library, false otherwise 
+bool IsInLibrary(const string &name) const;
+ 
+//Friend function 
+//ostream &operator<<(ostream &out, const library)
